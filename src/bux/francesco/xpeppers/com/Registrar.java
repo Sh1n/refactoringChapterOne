ba866050cd3f0429be49f37767e26e956ae5cbb9
shin <shin@fucinaludica.it>
2014-01-13 14:27:08 +0100

    CodeBase Initialized

---
+++ src/bux/francesco/xpeppers/com/Registrar.java
@@ -0,0 +1,20 @@
+package bux.francesco.xpeppers.com;
+
+public class Registrar {
+
+	public static Object get(String string, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void add(String string, Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void add(String string, Movie movie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/bux/francesco/xpeppers/com/Tape.java
@@ -0,0 +1,15 @@
+package bux.francesco.xpeppers.com;
+
+class Tape extends DomainObject {
+	public Movie movie() {
+		return _movie;
+	}
+
+	public Tape(String serialNumber, Movie movie) {
+		_serialNumber = serialNumber;
+		_movie = movie;
+	}
+
+	private String _serialNumber;
+	private Movie _movie;
+}
