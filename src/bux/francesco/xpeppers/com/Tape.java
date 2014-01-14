bd9473b22af94f33a2ed33eafa783173a43d9088
Francesco Shin Bux <francesco.bux@xpeppers.com>
2014-01-14 14:21:01 +0100

    Moved extractAmount to tape

--- src/bux/francesco/xpeppers/com/Tape.java
+++ src/bux/francesco/xpeppers/com/Tape.java
@@ -12,4 +12,26 @@ class Tape extends DomainObject {
 
 	private String _serialNumber;
 	private Movie _movie;
+	
+	public double extractAmount(int daysRented) {
+		// determine amounts for each line
+		double amount = 0;
+		switch (this.movie().priceCode()) {
+			case Movie.REGULAR:
+				amount += 2;
+				if (daysRented > 2)
+					amount += (daysRented - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				amount += daysRented * 3;
+				break;
+			case Movie.CHILDRENS:
+				amount += 1.5;
+				if (daysRented > 3)
+					amount += (daysRented - 3) * 1.5;
+				break;
+
+		}
+		return amount;
+	}
 }
